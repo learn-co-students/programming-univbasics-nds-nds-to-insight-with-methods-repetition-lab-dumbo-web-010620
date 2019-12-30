c8b25372e2a132e70ab0bab7d3b954bc4f04852e
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-30 03:45:53 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,15 +1,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
-def directors_totals(source)
+def directors_totals(source) #director_name = total_gross
+  # puts source
   result = {}
   director_index = 0
   while director_index < source.size do
     director = source[director_index]
-    result[director[:name]] = gross_for_director(director)
+    result[director[:name]] = gross_for_director(director) #gross_for_director is function to show worldwide_gross
     director_index += 1
   end
-  result
+ result
 end
 
 def gross_for_director(d)
@@ -24,8 +25,20 @@ def gross_for_director(d)
   total
 end
 
-def list_of_directors(source)
+def list_of_directors(source) #Returns Array of Hashes
   # Write this implementation
+  # puts source
+  array_directors = []
+  index = 0
+
+  while index  < source.length do
+    array_directors << source[index][:name]
+    index += 1
+  end
+
+  puts array_directors
+  array_directors
+
 end
 
 def total_gross(source)
@@ -38,6 +51,17 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  total_money = 0
+  directors_totals_map = directors_totals(source) #returns hash of names
+  director_names = list_of_directors(source)
+
+  index = 0
+  while index < director_names.length do
+    name = director_names[index]
+    total_money += directors_totals_map[name] #this is grabbing the number. [name] is to access value from hash
+    index += 1
+  end
+
+  puts total_money
+  total_money
 end
-
-
